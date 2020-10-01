9a593e3de576d96bc0fb79a7d53b4294c286458f
sammy <samuel.aj.page@gmail.com>
2020-10-01 21:26:24 +1000

    background enhancements
    - stored information into cookie 'bg_state'
    - better match of color to the night background

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,12 @@
 /* Table of Contents:
     1. Init
         1.1 main (background)
+            1.1a default background state (on)
     2. Functions
         2.1 logo
-        2.2 background
-        2.3 backgroundToggle*/
+        2.2 cookies
+        2.3 background
+        2.4 backgroundToggle*/
 /*main*/
 background("on"); /*matching background to the user time + respecting motion*/
 /*logo*/
@@ -20,23 +22,29 @@ function logo(id, content) {
         document.getElementById(id.id).title = 'click me';
     }
 }
+/*cookies*/
+function getCookie(name) {
+    var result = document.cookie.match("(^|[^;]+)\\s*" + name + "\\s*=\\s*([^;]+)");
+    return result ? result.pop() : "";
+}
 /*background*/
 function background(state) {
     var time = new Date();
     var hour = time.getHours();
+    if (getCookie("bg_state")) { // if true == if is set
+        state = getCookie("bg_state");
+    }
     if (hour < 6 || hour > 18) {
-        var background = ["url(assets/background-night.gif)", "darkgray"];
+        var background = ["url(assets/background-night.gif)", "#101010"];
     }
     else {
         var background = ["url(assets/background-day.gif)", "white"];
     }
     if (state == "off" || window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-        console.log(state);
         document.body.style.backgroundColor = background[1];
         document.body.style.backgroundImage = "url(#)";
     }
     else if (state == "on") {
-        console.log(state);
         document.body.style.backgroundImage = background[0];
         document.body.style.backgroundColor = "";
     }
@@ -44,9 +52,11 @@ function background(state) {
 /*backgroundToggle*/
 function backgroundToggle() {
     if (document.body.style.backgroundColor == "") {
+        document.cookie = "bg_state=off";
         background("off");
     }
     else {
+        document.cookie = "bg_state=on";
         background("on");
     }
 }
--- pre/app.ts
+++ pre/app.ts
@@ -1,17 +1,19 @@
 /* Table of Contents:
     1. Init
         1.1 main (background)
+            1.1a default background state (on)
     2. Functions
         2.1 logo
-        2.2 background
-        2.3 backgroundToggle*/
+        2.2 cookies
+        2.3 background
+        2.4 backgroundToggle*/
     
     
 /*main*/
 background("on"); /*matching background to the user time + respecting motion*/
 
 /*logo*/
-function logo(id,content) {
+function logo(id, content: string) {
     if (id.textContent !== content) {
         id.textContent = content;
         id.style.color = "darkcyan";
@@ -23,23 +25,31 @@ function logo(id,content) {
     }
 }
 
+/*cookies*/
+function getCookie(name: string) {
+    let result = document.cookie.match("(^|[^;]+)\\s*" + name + "\\s*=\\s*([^;]+)")
+    return result ? result.pop() : ""
+}
+
 /*background*/
 function background(state: string) {
     var time = new Date();
     var hour = time.getHours();
     
+    if (getCookie("bg_state")) { // if true == if is set
+        state = getCookie("bg_state");
+    }
+    
     if (hour < 6 || hour > 18) {
-        var background = ["url(assets/background-night.gif)","darkgray"];
+        var background = ["url(assets/background-night.gif)","#101010"];
     } else {
         var background = ["url(assets/background-day.gif)","white"];
     }
         
     if (state == "off" || window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-        console.log(state);
         document.body.style.backgroundColor = background[1];
         document.body.style.backgroundImage = "url(#)";
     } else if (state == "on") {
-        console.log(state);
         document.body.style.backgroundImage = background[0];
         document.body.style.backgroundColor = "";
     }
@@ -48,9 +58,10 @@ function background(state: string) {
 /*backgroundToggle*/
 function backgroundToggle() {
     if (document.body.style.backgroundColor == "") {
+        document.cookie = "bg_state=off";
         background("off");
     } else {
+        document.cookie = "bg_state=on";
         background("on");
     }
-}
-    
+}   
