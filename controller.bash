bac233a4d793789021419b70709028e02e379a54
sammy <samuel.aj.page@gmail.com>
2020-10-06 16:45:29 +1000

    [refactor one] controller.bash
    - Removed some of the non-local features, currently has no jq operations (besides installing jq if it's missing).
    - Seperated Sections into relevant categories.
    - Moved code from the switch-case to functions, the main section mostly just references the relevant function.
    - Removed the common ``if [[ x ==  ]]...`` because it's the same as ``if [[ ! x ]]...``
    - Organised help to be more dynamic, with less code.
    - Removed $intendedStr to instead use ${var#unwanted_fluff} directly.
    - Created an ongoing style of receipt messages.
    - Other small changes to increase general quality

--- controller.bash
+++ controller.bash
@@ -1,53 +1,111 @@
-mp3dl="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s'";
-help="help, $(tput bold)exit, list, remove, play [song/url], clean$(tput sgr0)"
+# *) Globals
+musicDir="./assets/music"
 
+# Load) Globals
+mp3dl="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s'"
+
+# Help) Globals
+help=("help" "exit" "list" "load" "remove" "start" "stop")
+tip=("outputs this message, try 'help [option]'" "exits the controller and sync process" "shows the queued and the loaded songs" "parses songs from youtube, try 'load [song name]'" "removes loaded songs, try 'remove [song/all]'" "moves songs from the queue to the play-state" "stops streaming to murb")
+
+# Dependencies
+if ! [[ `pip3 show youtube-dl` ]]; then
+    sudo apt-get install pip3
+    pip3 install youtube-dl
+    sudo apt-get install ffmpeg
+fi
+
+if ! [[ `dpkg -s jq` ]]; then
+    sudo apt-get install jq
+fi
+
+# Functions
+function remove() {
+    directory=$1; file=$2
+    targetFile=`ls $directory | grep -i -m1 "$file"` # Find a matching file to input
+    if [[ $targetFile ]]; then
+        read -p "Enter 'yes' to delete $(tput bold)$targetFile$(tput sgr0): " answer
+        if [[ `echo $answer | tr [:upper:] [:lower:]` == "yes" ]]; then # Translates $answer to lowercase
+            rm -v "$directory/$targetFile"
+            echo -e "temp: [$(tput setaf 2)deleted$(tput sgr0)]\n"
+        else
+            echo -e "[$(tput setaf 1)cancelled$(tput sgr0)]\n"
+        fi
+    else
+        echo -e "[$(tput setaf 1)unable to find song$(tput sgr0)]\n"
+    fi
+}
+
+function list() {
+    echo -e "$(tput bold)loaded:$(tput sgr0) \n`ls ./assets/music`\n"
+    echo -e "$(tput bold)queued:$(tput sgr0) \n[jq command]\n"
+}
+
+function help() {
+    option=$1
+    option=`echo $option | tr [:upper:] [:lower:]` # Translates $option to lowercase
+    i=0; for each in ${help[@]}
+    do
+        if [[ $option == ${help[i]} ]]; then # Only producing the correct help information
+            savedOption=${help[i]} # Saving the option for later
+            help[i]="$(tput bold)${help[i]}$(tput sgr0)" # Adjusting the array to make the current option bold
+            echo -e "${help[@]}\n$(tput dim)- ${tip[i]}$(tput sgr0)\n" # Outputting the modified array and the matching tip
+            help[i]="$savedOption" # Returning the array to normal
+        fi
+        ((i++))
+    done
+}
+
+function load() {
+    query=$1
+    if [[ $query ]]; then
+        $mp3dl "ytsearch:$query" # Temporarily store the song in the root
+        file=`ls | grep ".mp3"` # Find the song
+        if [[ $file ]]; then
+            mv $file ./assets/music # Move to correct place
+            echo -e "\n[stored $(tput setaf 4)$file$(tput sgr0)]\n"
+            read -p "Enter 'yes' to queue $(tput bold)$file$(tput sgr0): " answer
+            if [[ `echo $answer | tr [:upper:] [:lower:]` == "yes" ]]; then
+                #queue $file
+                echo -e "\n[queued $(tput setaf 4)$file$(tput sgr0)]\n"
+            fi
+        else
+            echo -e "[$(tput setaf 1)cancelled$(tput sgr0)]\n"
+        fi
+    else
+        echo -e "load $(tput setaf 1)[name/url]$(tput sgr0)\n"
+    fi
+}
+
+# Main
+clear
 while true
 do
-    read -p "input: " input
+    read -p "command: " input
     clear
     case $input in
         exit)
             exit
+            #syncProcess "off"
         ;;
-        help)
-            echo $help
+        help*)
+            if [[ $input == "help" ]]; then
+                help "$input"
+            else
+                help "${input#help}"
+            fi
         ;;
         list|ls)
-            if [[ `ls ./assets/music` == "" ]]
-            then
-                echo "$(tput setaf 1)song dir empty$(tput sgr0)"
-            else
-                ls ./assets/music
-            fi
+            list
         ;;
         remove*)
-            #remove from queueueeu
-            intendedStr=${input#remove } # Remove 'remove ' from $input
-            echo "intended: $intendedStr"
-            correctedStr=`ls ./assets/music | grep -i -m1 "$intendedStr"` # i = case insensitive, m1 = only return one line   
-            echo "search query: $correctedStr"
-            echo "removed: $correctedStr"   
-            rm ./assets/music/"$correctedStr"
+            remove "$musicDir" "${input#remove}"
         ;;
-        play*)
-            intendedStr=${input#play } # Remove 'song ' from $input
-            $mp3dl "ytsearch:$intendedStr"
-            mv *.mp3 ./assets/music
-            clear
-            if [[ $intendedStr == "" ]]
-            then
-                echo "song $(tput setaf 1)[name/url]$(tput sgr0)"
-            else
-                echo "$(tput bold)song stored: $(tput sgr0)$(tput setaf 4)`ls ./assets/music | grep -i "$intendedStr"`$(tput sgr0)"
-            fi
-            #add to queue
-        ;;
-        clean)
-            rm ./assets/music/* -v
-            #clean current/queue
+        load*)
+            load "${input#load}"
         ;;
         *)
-            echo "$(tput setaf 1)unknown input$(tput sgr0)"
-            echo "$(tput bold)$help$(tput sgr0)"
+            echo -e "[$(tput setaf 1)unknown command$(tput sgr0)]\ntry 'help'\n"
+        ;;
     esac
 done
