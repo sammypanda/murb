278d28e22ed4f4b2d3b617e9fd4cfebf769b436e
samdvich <sammy0penguin@gmail.com>
2020-12-11 11:34:52 +1000

    added load command w/ ytdl

--- controller.py
+++ controller.py
@@ -21,8 +21,14 @@ scriptDir = os.path.dirname(os.path.realpath(__file__))
 musicDir = scriptDir + "/assets/music"
 
 class color:
-   BOLD = '\033[1m'
-   END = '\033[0m'
+    RED = '\033[31m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+def ytdl_hook(d):
+    if d['status'] == 'finished':
+        global loadedfile
+        loadedfile = d['filename']
 
 # Options) Globals
 ytdl_opts = {
@@ -33,12 +39,14 @@ ytdl_opts = {
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
     }],
+    'progress_hooks': [ytdl_hook],
 }
 
 # Menu) Help
 helpTitle = ("help*", "exit", "list", "load*", "remove*", "play*", "start", "stop", "skip*")
 helpTip = ("outputs this message, try 'help [option]'", "exits the controller and sync process", "shows the queued and the loaded songs", "parses songs from youtube, try 'load [song name]'", "removes loaded songs, try 'remove [song]'", "plays loaded songs, try 'play [song]'", "moves songs from the queue to the play-state", "stops streaming to murb", "skips the current song, try 'skip [queue song index]'")
 
+# Functions)
 def help(option=""):
     if option:
         option = option.lower()
@@ -59,6 +67,17 @@ def exit():
             os.remove(file)
     quit()
 
+def load(song):
+    if song:
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+            ytdl.download([song])
+            if 'loadedfile' in globals():
+                print(loadedfile)
+            else:
+                load(song)
+    else:
+        help("load")
+
 while True:
     print()
     command = input("command: ")
@@ -68,6 +87,11 @@ while True:
         os.system('clear') 
 
     if "help" in command:
-        help(command.replace('help ',''))
+        help(command.replace('help','').strip())
     elif "exit" in command:
         exit()
+    elif "load" in command:
+        load(command.replace('load','').strip())
+    else:
+        print("[ " + color.RED + "Unknown Command" + color.END + " ]" + "\n")
+        help()
