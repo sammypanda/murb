696869d7c18c098681f323938fb763f3e40df4f4
samdvich <sammy0penguin@gmail.com>
2020-12-11 10:08:40 +1000

    added help command

--- controller.py
+++ controller.py
@@ -1,11 +1,7 @@
 # Table of Contents:
     #1 Variables
         #1a Globals
-        #1b Load
         #1c Help
-    #2 Dependencies
-        #2a Youtube-DL
-        #2b JQ
     #3 Functions
         #3a syncActivity()
         #3b song()
@@ -24,6 +20,10 @@ import os, youtube_dl
 scriptDir = os.path.dirname(os.path.realpath(__file__))
 musicDir = scriptDir + "/assets/music"
 
+class color:
+   BOLD = '\033[1m'
+   END = '\033[0m'
+
 # Options) Globals
 ytdl_opts = {
     'format': 'bestaudio/best',
@@ -35,5 +35,31 @@ ytdl_opts = {
     }],
 }
 
-with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
-    ytdl.download(["test"])
+# Menu) Help
+helpTitle = ("help*", "exit", "list", "load*", "remove*", "play*", "start", "stop", "skip*")
+helpTip = ("outputs this message, try 'help [option]'", "exits the controller and sync process", "shows the queued and the loaded songs", "parses songs from youtube, try 'load [song name]'", "removes loaded songs, try 'remove [song]'", "plays loaded songs, try 'play [song]'", "moves songs from the queue to the play-state", "stops streaming to murb", "skips the current song, try 'skip [queue song index]'")
+
+def help(option=""):
+    if option:
+        option = option.lower()
+    else:
+        option = "help"
+
+    i = 0
+    for each in helpTitle:
+        if option == each.replace('*',''):
+            print(color.BOLD + each + " - " + helpTip[i] + color.END)
+        else:
+            print(each)
+        i = i + 1
+
+while True:
+    print()
+    command = input("command: ")
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear') 
+
+    if "help" in command:
+        help(command.replace('help ',''))
